d672205ad4769338b2da9435c5b32dc7f44ee985
Seyun <ksy90101@gmail.com>
2020-03-05 13:04:41 +0900

    [feat] 슬랙봇 장르별대로 노동요 추천 기능 구현

--- index.js
+++ index.js
@@ -1,103 +1,293 @@
 require('dotenv').config();
 
-// let songs = ['https://www.youtube.com/playlist?list=PLl0rRg6gLsHYO_ys6ULX9uxRIwMa6_q-a',
-//     'https://www.youtube.com/watch?v=o_UUYwymh30',
-//     'https://www.youtube.com/watch?v=_wfvK8IT33o']
-
-// function shuffle(songs) {
-//     var j, x, i;
-//     for (i = songs.length; i; i -= 1) {
-//         j = Math.floor(Math.random() * i);
-//         x = songs[i - 1];
-//         songs[i - 1] = songs[j];
-//         songs[j] = x;
-//     }
-// }
-
-// var RtmClient = require('slack-client').RtmClient;
-// var WebClient = require('slack-client').WebClient;
-// var token = process.env.BOT_TOKEN;
-
-// var web = new WebClient(token);
-// var rtm = new RtmClient(token, { logLevel: 'error' });
-// rtm.start();
-
-// var RTM_EVENTS = require('slack-client').RTM_EVENTS;
-
-// // rtm 모듈이 메세지를 받았을 때 반응
-// rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-//     var channel = message.channel;
-//     var user = message.user;
-//     var text = message.text;
-
-//     shuffle(songs);
-
-//     if (text == '노래')
-//         web.chat.postMessage(channel, songs[0], { username: "노동요" });
-// });
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-
+const axios = require('axios');
+const qs = require('qs');
 const app = express();
-const Song = require('./models/song');
-const router = require('./routes')(app, Song);
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function () {
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://rutgo:rutgo1@ds221990.mlab.com:21990/heroku_nc9pp90w');
+
+const song = require('./models/song');
 
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('3000 PORT OPEN');
-})
+});
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-    var data = {
-        token: process.env.BOT_TOKEN,
-        channel: "노동요",
-        text: "",
-        blocks: [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "생일을 등록하면 알려줄게요!"
-                }
-            },
-            {
-                "type": "actions",
-                "elements": [
+app.post('/events', (req, res) => {
+
+    if (req.body.challenge && req.body.type == "url_verification") {
+        res.json({challenge: req.body.challenge});
+    }
+    if (req.body.event.type === 'app_mention') {
+        appMentionEventBlocks(req.body.event.channel);
+        res.json('');
+    }
+});
+
+app.post(`/actions`, (req, res) => {
+    const {token, trigger_id, user, actions, type, channel} = JSON.parse(req.body.payload);
+    if (actions && actions[0].action_id.match(`add_song`)) {
+        addSongOpenModal(trigger_id);
+    } else if (actions && actions[0].action_id.match('give_song')) {
+        giveSongOpenModal(trigger_id);
+    } else if (type === 'view_submission') {
+        const genre = JSON.parse(req.body.payload).view.state.values.songSelect.songValue.selected_option.value;
+        send("테스트채널", genre);
+        res.send({response_action:"clear"});
+    }
+});
+
+const appMentionEventBlocks = async (channel) => {
+    const modal = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "노래를 등록하시거나, 노래를 추천해드립니다~"
+            }
+        },
+        {
+            "type":
+                "actions",
+            "elements":
+                [
                     {
                         "type": "button",
+                        "action_id": "add_song",
                         "text": {
                             "type": "plain_text",
-                            "text": "생일 등록하기",
+                            "text": "노래 등록하기",
                             "emoji": true
                         },
-                        "value": "click_me_123"
+                        "value": "click_me_123",
                     },
                     {
                         "type": "button",
+                        "action_id": "give_song",
                         "text": {
                             "type": "plain_text",
-                            "text": "이번달 생일자 보러가기",
+                            "text": "노래 추천 받기",
                             "emoji": true
                         },
-                        "response_url": "https://a667d3d6.ngrok.io/songs"
                     }
                 ]
+        }
+    ];
+    const args = {
+        token: `xoxb-952248750261-966576957937-pcmQT7FClIeDMbddQttpfi69`,
+        channel: channel,
+        text: "노래 추천 해드릴께요",
+        blocks: JSON.stringify(modal)
+    };
+    const result = await axios.post(`https://slack.com/api/chat.postMessage`, qs.stringify(args));
+};
+
+const addSongOpenModal = async (trigger_id) => {
+    const modal = {
+        "type": "modal",
+        "title": {
+            "type": "plain_text",
+            "text": "My App",
+            "emoji": true
+        },
+        "submit": {
+            "type": "plain_text",
+            "text": "Submit",
+            "emoji": true
+        },
+        "close": {
+            "type": "plain_text",
+            "text": "Cancel",
+            "emoji": true
+        },
+        "blocks": [
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "링크",
+                    "emoji": true
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "plain_text_input"
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "한줄평",
+                    "emoji": true
+                }
+            },
+            {
+                "type": "input",
+                "element": {
+                    "type": "static_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "장르",
+                        "emoji": true
+                    },
+                    "options": [
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "*신나는 곡*",
+                                "emoji": true
+                            },
+                            "value": "exciting"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "*차분한곡*",
+                                "emoji": true
+                            },
+                            "value": "quiet"
+                        }
+                    ]
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "장르",
+                    "emoji": true
+                }
             }
         ]
     };
 
-    request.post('https://slack.com/api/chat.postMessage', function (error, response, body) {
-        res.json(data);
-    });
-});
+    const args = {
+        token: `xoxb-952248750261-966576957937-pcmQT7FClIeDMbddQttpfi69`,
+        trigger_id: trigger_id,
+        view: JSON.stringify(modal)
+    };
+    const result = await axios.post(`https://slack.com/api/views.open`, qs.stringify(args));
+};
 
-app.post('/verify', (req, res) => {
-    if (req.body.challenge && req.body.type == "url_verification") {
-        res.json({ challenge: req.body.challenge });
-    }
-})
+const giveSongOpenModal = async (trigger_id) => {
+    const modal = {
+        "type": "modal",
+        "title": {
+            "type": "plain_text",
+            "text": "My App",
+            "emoji": true
+        },
+        "submit": {
+            "type": "plain_text",
+            "text": "Submit",
+            "emoji": true
+        },
+        "close": {
+            "type": "plain_text",
+            "text": "Cancel",
+            "emoji": true
+        },
+        "blocks": [
+            {
+                "type": "input",
+                "block_id": "songSelect",
+                "element": {
+                    "type": "static_select",
+                    "action_id": "songValue",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select an item",
+                        "emoji": true
+                    },
+                    "options": [
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "신나는곡",
+                                "emoji": true
+                            },
+                            "value": "exciting"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "차분한곡",
+                                "emoji": true
+                            },
+                            "value": "quiet"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "우울 그자체",
+                                "emoji": true
+                            },
+                            "value": "value-2"
+                        }
+                    ]
+                },
+                "label": {
+                    "type": "plain_text",
+                    "text": "Label",
+                    "emoji": true
+                }
+            }
+        ]
+    };
+
+    const args = {
+        token: `xoxb-952248750261-966576957937-pcmQT7FClIeDMbddQttpfi69`,
+        trigger_id: trigger_id,
+        view: JSON.stringify(modal)
+    };
+    const result = await axios.post(`https://slack.com/api/views.open`, qs.stringify(args));
+};
+
+const send = async (channel, genre) => {
+    var songList = [];
+    song.find({genre: genre.toString()}, {_id: false, genre: false}, function (err, songs) {
+        for (var value in songs) {
+            songList.push("링크 : " + songs[value].link + "\n" + "추천이유 : " + songs[value].review);
+        }
+        function shuffle(songList) {
+            var j, x, i;
+            for (i = songList.length; i; i -= 1) {
+                j = Math.floor(Math.random() * i);
+                x = songList[i - 1];
+                songList[i - 1] = songList[j];
+                songList[j] = x;
+            }
+        }
+
+        shuffle(songList);
+
+        text = songList[0];
+
+        const args = {
+            token: `xoxb-952248750261-966576957937-pcmQT7FClIeDMbddQttpfi69`,
+            channel: channel,
+            text: text
+        };
+
+        const result = axios.post(`https://slack.com/api/chat.postMessage`, qs.stringify(args));
+
+        try {
+            console.log(result.data);
+        } catch (e) {
+            console.log(e);
+        }
+    });
+};
