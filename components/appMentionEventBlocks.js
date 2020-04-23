e15c366ffd006fa07d2c8e87fed48333701c9c6b
Seyun <ksy90101@gmail.com>
2020-04-23 17:48:34 +0900

    refactor : 로그용 출력

--- components/appMentionEventBlocks.js
+++ components/appMentionEventBlocks.js
@@ -4,58 +4,73 @@ const qs = require('qs');
 const appMentionEventBlocks = async (channel, token) => {
     console.log("함수 내 channel : " + channel);
     const modal = [
-        {
-            "type":
-                "actions",
-            "elements":
-                [
-                    {
-                        "type": "button",
-                        "action_id": "add_song",
-                        "text": {
-                            "type": "plain_text",
-                            "text": ":musical_keyboard: 노래 등록",
-                            "emoji": true
-                        },
-                        "value": "click_me_123",
+            {
+                "type":
+                    "actions",
+                "elements": [{
+                    "type": "button",
+                    "action_id": "add_song",
+                    "text": {
+                        "type": "plain_text",
+                        "text": ":musical_keyboard: 노래 등록",
+                        "emoji": true
                     },
-                    {
-                        "type": "button",
-                        "action_id": "give_song",
-                        "text": {
-                            "type": "plain_text",
-                            "text": ":musical_note: 노래 추천",
-                            "emoji": true
-                        },
-                    },
-                    {
-                        "type": "button",
-                        "action_id": "add_study",
-                        "text": {
-                            "type": "plain_text",
-                            "text": ":male-technologist: 스터디 자료 등록",
-                            "emoji": true
-                        },
-                        "value": "click_me_123",
-                    },
-                    {
-                        "type": "button",
-                        "action_id": "give_study",
-                        "text": {
-                            "type": "plain_text",
-                            "text": ":female-technologist: 스터디 자료 추천",
-                            "emoji": true
-                        },
-                    }
-                ]
-        }
-    ];
+                    "value": "click_me_123"
+                }]
+            }
+            // {
+            //     "type":
+            //         "actions",
+            //     "elements":
+            //         [
+            //             {
+            //                 "type": "button",
+            //                 "action_id": "add_song",
+            //                 "text": {
+            //                     "type": "plain_text",
+            //                     "text": ":musical_keyboard: 노래 등록",
+            //                     "emoji": true
+            //                 },
+            //                 "value": "click_me_123",
+            //             },
+            //             {
+            //                 "type": "button",
+            //                 "action_id": "give_song",
+            //                 "text": {
+            //                     "type": "plain_text",
+            //                     "text": ":musical_note: 노래 추천",
+            //                     "emoji": true
+            //                 },
+            //             },
+            //             {
+            //                 "type": "button",
+            //                 "action_id": "add_study",
+            //                 "text": {
+            //                     "type": "plain_text",
+            //                     "text": ":male-technologist: 스터디 자료 등록",
+            //                     "emoji": true
+            //                 },
+            //                 "value": "click_me_123",
+            //             },
+            //             {
+            //                 "type": "button",
+            //                 "action_id": "give_study",
+            //                 "text": {
+            //                     "type": "plain_text",
+            //                     "text": ":female-technologist: 스터디 자료 추천",
+            //                     "emoji": true
+            //                 },
+            //             }
+            //         ]
+            // }
+        ]
+    ;
     const args = {
         token: token,
         channel: channel,
         blocks: JSON.stringify(modal)
     };
-    const result = await axios.post(`https://slack.com/api/chat.postMessage`, qs.stringify(args));
+    const result = await axios.post('https://slack.com/api/chat.postMessage', qs.stringify(args));
 
     console.log(result.status);
     console.log(result.statusText);
