ea47de68f1f907595c9e74db119814364660a316
Seyun <ksy90101@gmail.com>
2020-04-23 17:53:21 +0900

    refactor : 모달 변경

--- components/appMentionEventBlocks.js
+++ components/appMentionEventBlocks.js
@@ -4,67 +4,40 @@ const qs = require('qs');
 const appMentionEventBlocks = async (channel, token) => {
     console.log("함수 내 channel : " + channel);
     const modal = [
-            {
-                "type":
-                    "actions",
-                "elements": [{
-                    "type": "button",
-                    "action_id": "add_song",
-                    "text": {
-                        "type": "plain_text",
-                        "text": ":musical_keyboard: 노래 등록",
-                        "emoji": true
-                    },
-                    "value": "click_me_123"
-                }]
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "노래를 등록하시거나, 노래를 추천해드립니다~"
             }
-            // {
-            //     "type":
-            //         "actions",
-            //     "elements":
-            //         [
-            //             {
-            //                 "type": "button",
-            //                 "action_id": "add_song",
-            //                 "text": {
-            //                     "type": "plain_text",
-            //                     "text": ":musical_keyboard: 노래 등록",
-            //                     "emoji": true
-            //                 },
-            //                 "value": "click_me_123",
-            //             },
-            //             {
-            //                 "type": "button",
-            //                 "action_id": "give_song",
-            //                 "text": {
-            //                     "type": "plain_text",
-            //                     "text": ":musical_note: 노래 추천",
-            //                     "emoji": true
-            //                 },
-            //             },
-            //             {
-            //                 "type": "button",
-            //                 "action_id": "add_study",
-            //                 "text": {
-            //                     "type": "plain_text",
-            //                     "text": ":male-technologist: 스터디 자료 등록",
-            //                     "emoji": true
-            //                 },
-            //                 "value": "click_me_123",
-            //             },
-            //             {
-            //                 "type": "button",
-            //                 "action_id": "give_study",
-            //                 "text": {
-            //                     "type": "plain_text",
-            //                     "text": ":female-technologist: 스터디 자료 추천",
-            //                     "emoji": true
-            //                 },
-            //             }
-            //         ]
-            // }
-        ]
-    ;
+        },
+        {
+            "type":
+                "actions",
+            "elements":
+                [
+                    {
+                        "type": "button",
+                        "action_id": "add_song",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "노래 등록하기",
+                            "emoji": true
+                        },
+                        "value": "click_me_123",
+                    },
+                    {
+                        "type": "button",
+                        "action_id": "give_song",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "노래 추천 받기",
+                            "emoji": true
+                        },
+                    }
+                ]
+        }
+    ];
     const args = {
         token: token,
         channel: channel,
