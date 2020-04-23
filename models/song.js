2d03ab5f66fb72397f9bda98aac09741d37f8bfd
Seyun <ksy90101@gmail.com>
2020-04-23 19:50:27 +0900

    refactor : 노래 번호 컬럼 추가

--- models/song.js
+++ models/song.js
@@ -1,11 +1,21 @@
 var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+
 var Schema = mongoose.Schema;
 
 var songSchema = new Schema({
+    no: {type: number, required: true, unique: true}
     link: {type: String, required: true, unique: true},
     user: {type: String, required: true},
     genre: {type: String, required: true},
     review: {type: String, required: true}
+});
+
+songSchema.plugin(autoIncrement.plugin, {
+    model: 'song',
+    field: 'no',
+    startAt: 1,
+    increment: 1
 });
 
 module.exports = mongoose.model('song', songSchema);
