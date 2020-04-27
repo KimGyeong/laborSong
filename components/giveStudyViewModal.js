3d9406aa7744910e9235f7a6724c2d159cff584a
Seyun <ksy90101@gmail.com>
2020-04-27 17:26:32 +0900

    refactor : modal 수정

--- components/giveStudyViewModal.js
+++ components/giveStudyViewModal.js
@@ -5,92 +5,48 @@ const studies = require('../models/study');
 
 const giveStudyViewModal = async (trigger_id, token, level) => {
 
-    studies.find({level: level.toString()}, {_id: false, link: false}, function (err, studies) {
-        console.log("studies : " + studies);
+        studies.find({level: level.toString()}, {_id: false, link: false}, function (err, studies) {
+            console.log("studies : " + studies);
 
-        let study_field = "[";
+            let study_field = "[";
 
-        for (let value in studies) {
-            textValue = (" 제목 : "
-                + studies[value].title
-                + "\n"
-                + " 링크 : "
-                + studies[value].link
-                + "\n");
+            for (let value in studies) {
+                textValue = (" 제목 : "
+                    + studies[value].title
+                    + "\n"
+                    + " 링크 : "
+                    + studies[value].link
+                    + "\n");
 
-            study_field += (
-                "{"
-                + "\"type\" : \"plain_text\","
-                + "\"text\" : "
-                + textValue
-                + "\"emoji\" : true"
-                + "}"
-            )
-            // tempResult +=
-            //     ("레벨 : "
-            //         + studies[value].level
-            //         + "\n"
-            //         + " 제목 : "
-            //         + studies[value].title
-            //         + "\n"
-            //         + " 링크 : "
-            //         + studies[value].link
-            //         + "\n");
-            // console.log("tempResult in for loop : " + tempResult);
-        }
+                study_field += (
+                    "{"
+                    + "\"type\" : \"plain_text\","
+                    + "\"text\" : "
+                    + textValue
+                    + "\"emoji\" : true"
+                    + "}"
+                )
+                // tempResult +=
+                //     ("레벨 : "
+                //         + studies[value].level
+                //         + "\n"
+                //         + " 제목 : "
+                //         + studies[value].title
+                //         + "\n"
+                //         + " 링크 : "
+                //         + studies[value].link
+                //         + "\n");
+                // console.log("tempResult in for loop : " + tempResult);
+            }
 
-        study_field = "[\n" +
-            "\t\t\t\t{\n" +
-            "\t\t\t\t\t“type”: “plain_text”,\n" +
-            "\t\t\t\t\t“text”: “*this is plain_text text*“,\n" +
-            "\t\t\t\t\t“emoji”: true\n" +
-            "\t\t\t\t},\n" +
-            "\t\t\t\t{\n" +
-            "\t\t\t\t\t“type”: “plain_text”,\n" +
-            "\t\t\t\t\t“text”: “*this is plain_text text*“,\n" +
-            "\t\t\t\t\t“emoji”: true\n" +
-            "\t\t\t\t},\n" +
-            "\t\t\t\t{\n" +
-            "\t\t\t\t\t“type”: “plain_text”,\n" +
-            "\t\t\t\t\t“text”: “*this is plain_text text*“,\n" +
-            "\t\t\t\t\t“emoji”: true\n" +
-            "\t\t\t\t},\n" +
-            "\t\t\t\t{\n" +
-            "\t\t\t\t\t“type”: “plain_text”,\n" +
-            "\t\t\t\t\t“text”: “*this is plain_text text*“,\n" +
-            "\t\t\t\t\t“emoji”: true\n" +
-            "\t\t\t\t},\n" +
-            "\t\t\t\t{\n" +
-            "\t\t\t\t\t“type”: “plain_text”,\n" +
-            "\t\t\t\t\t“text”: “*this is plain_text text*“,\n" +
-            "\t\t\t\t\t“emoji”: true\n" +
-            "\t\t\t\t}\n" +
-            "\t\t\t]";
+            study_field = "";
 
-        const modal = {
-            "type": "modal",
-
-            "title": {
-                "type": "plain_text",
-                "text": "My App",
-                "emoji": true
-            },
-            "submit": {
-                "type": "plain_text",
-                "text": "Submit",
-                "emoji": true
-            },
-            "close": {
-                "type": "plain_text",
-                "text": "Cancel",
-                "emoji": true
-            },
-            "blocks": [
+            const modal = [
                 {
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": "스터디 자료를 추천해줍니다!"
+                        "text": "This is a mrkdwn section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>"
                     }
                 },
                 {
@@ -98,31 +54,58 @@ const giveStudyViewModal = async (trigger_id, token, level) => {
                 },
                 {
                     "type": "section",
-                    "fields": study_field
+                    "fields": [
+                        {
+                            "type": "plain_text",
+                            "text": "*this is plain_text text*",
+                            "emoji": true
+                        },
+                        {
+                            "type": "plain_text",
+                            "text": "*this is plain_text text*",
+                            "emoji": true
+                        },
+                        {
+                            "type": "plain_text",
+                            "text": "*this is plain_text text*",
+                            "emoji": true
+                        },
+                        {
+                            "type": "plain_text",
+                            "text": "*this is plain_text text*",
+                            "emoji": true
+                        },
+                        {
+                            "type": "plain_text",
+                            "text": "*this is plain_text text*",
+                            "emoji": true
+                        }
+                    ]
                 }
             ]
-        };
 
-        console.log(modal);
+            console.log(modal);
 
-        const args = {
-            token: token,
-            trigger_id: trigger_id,
-            view: JSON.stringify(modal)
-        };
+            const args = {
+                token: token,
+                trigger_id: trigger_id,
+                view: JSON.stringify(modal)
+            };
 
-        const result = axios.post(`https://slack.com/api/views.open`, qs.stringify(args));
+            const result = axios.post(`https://slack.com/api/views.open`, qs.stringify(args));
 
-        result.then(response =>{
-            //console.log(response);
-            console.log(response.text());
-        }
-        ).catch(
-            error =>
-                console.log(error)
-        );
+            result.then(response => {
+                    //console.log(response);
+                    console.log(response.text());
+                }
+            ).catch(
+                error =>
+                    console.log(error)
+            );
 
-    });
-};
+        })
+        ;
+    }
+;
 
 module.exports = giveStudyViewModal;
