eb65485d0adc734058666783b180cdc30a9a8daa
Seyun <ksy90101@gmail.com>
2020-05-04 15:28:29 +0900

    refactor : 좋아요 싫어요 심볼 추가

--- components/giveSongViewModal.js
+++ components/giveSongViewModal.js
@@ -2,19 +2,10 @@ const axios = require('axios');
 const qs = require('qs');
 
 const songs = require('../models/song');
+const shuffle = require('../utils/shuffle');
 
 const giveSongViewModal = (trigger_id, token, genre) => {
     songs.find({genre: genre.toString()}, {_id: false, genre: false}, async function (error, songs) {
-
-        function shuffle(songList) {
-            var j, x, i;
-            for (i = songList.length; i; i -= 1) {
-                j = Math.floor(Math.random() * i);
-                x = songList[i - 1];
-                songList[i - 1] = songList[j];
-                songList[j] = x;
-            }
-        }
 
         shuffle(songs);
 
@@ -30,7 +21,7 @@ const giveSongViewModal = (trigger_id, token, genre) => {
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": songs[0].review.toString()
+                        "text": songs[0].review.toString() + ":thumbsup:" + ":thumbsdown:"
                     },
                     "accessory": {
                         "type": "button",
@@ -45,7 +36,7 @@ const giveSongViewModal = (trigger_id, token, genre) => {
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": songs[1].review.toString()
+                        "text": songs[1].review.toString() + ":thumbsup:" + ":thumbsdown:"
                     },
                     "accessory": {
                         "type": "button",
@@ -60,7 +51,7 @@ const giveSongViewModal = (trigger_id, token, genre) => {
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": songs[2].review.toString()
+                        "text": songs[2].review.toString() + ":thumbsup:" + ":thumbsdown:"
                     },
                     "accessory": {
                         "type": "button",
@@ -81,7 +72,7 @@ const giveSongViewModal = (trigger_id, token, genre) => {
             view: JSON.stringify(modal)
         };
 
-        const result = await axios.post(`https://slack.com/api/views.push`, qs.stringify(args));
+        const result = await axios.post('https://slack.com/api/views.push', qs.stringify(args));
 
         console.log(result.data);
         console.log(result.data.response_metadata);
